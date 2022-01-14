c44d9104302d64fcdb06402972a3937d8f4d60e9
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2022-01-14 13:24:07 +0700

    TUGAS UAS ALGORITMA PEMROGRAMAN SEMESTER I

---
+++ Annisa Nur Rachmawati/soal2.py
@@ -0,0 +1,40 @@
+
+
+print("\n")
+print("Nama \t : Annisa Nur Rachmawati\n")
+print("NIM \t : 21081000005\n")
+print("Kelas \t : 1D\n")
+# cetak judul program
+print('program menentukan nilai maksimum tiga bilangan Cara Pertama\n')
+ 
+# input user
+
+# menyiapkan variabel untuk bilangan pertama
+a = int(input('masukan bilangan ke-1: '))
+
+# menyiapkan variabel untuk bilangan kedua
+b = int(input('masukan bilangan ke-2: '))
+
+# menyiapkan variabel untuk bilangan ketiga
+c = int(input('masukan bilangan ke-3: '))
+ 
+# menenukan nilai terbesar
+
+# memeriksa apakah variabel a lebih besar dari variabel b
+if a > b:
+    # jika iya maka variabel maks diisi variabel a
+    maks = a
+else :
+    # jika tidak maka variabel maks diisi variabel b
+    maks = b
+
+# memeriksa apakah variabel maks lebih besar dari variabel c
+if maks > c :
+    # jika iya maka variabel maks tetap
+    maks = maks
+else :
+    # jika tidak maka variabel maks diisi variabel c
+    maks = c
+ 
+# cetak bilangan terbesar
+print('Nilai yang terbesar adalah: %d' % maks)
---
+++ Ayyub Hasanul Khuluq/soal2.py
@@ -0,0 +1,24 @@
+
+print("\n")
+print("Nama\t : Ayyub Hasanul Khuluq\n")
+print("NIM\t : 21081000009\n")
+print("Kelas\t : 1D\n")
+# cetak judul program
+print('program menentukan nilai maksimum tiga bilangan Cara Pertama\n')
+
+# input user
+a = int(input('masukan bilangan ke-1: '))
+b = int(input('masukan bilangan ke-2: '))
+c = int(input('masukan bilangan ke-3: '))
+
+# menenukan nilai terbesar
+if a > b:  # memeriksa apakah nilai a lebih besar dari pada b
+    if a > c:  # memeriksa apakah nilai a lebih besar dari pada c
+        maks = a  # jika iya variabel max di isi variabel a
+else:
+    if b > c:  # memeriksa apakah nilai b lebih besar dari pada c
+        maks = b  # jika iya variabel max di isi variabel b
+    else:
+        maks = c  # jika iya variabel max di isi variabel c
+
+print('Nilai yang terbesar adalah: %d' % maks)
