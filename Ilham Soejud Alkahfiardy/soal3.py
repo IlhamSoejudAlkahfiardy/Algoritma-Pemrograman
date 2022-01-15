02551975d01bda1bee97e76c9682b50b350d3e70
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2022-01-15 19:56:11 +0700

    TUGAS UAS ALGORITMA PEMROGRAMAN SEMESTER I

---
+++ Ilham Soejud Alkahfiardy/soal3.py
@@ -0,0 +1,16 @@
+# data diri
+print("Nama \t : Ilham Soejud Alkahfiardy")
+print("NIM \t : 21081000005")
+print("Kelas \t : 1D\n")
+
+# judul program
+print("Menjumlahkan 2 array dengan dimensi yang sama")
+
+# membuat 2 variabel array
+array1 = [1, 3, 4]
+array2 = [3, 4, 1]
+
+# menjumlahkan array1 dengan array2 sesuai dimensinya
+array3 = [array1[0] + array2[0], array1[1] + array2[1], array1[2] + array2[2]]
+
+print("isi dari array 3 adalah : " + str(array3))
